934538cf747bfa719a78b2fbdd1261b27769bc75
Benjamin Jones <Benjamin.jones@eriasventures.com>
2024-08-15 17:39:07 +0000

    More cleanup

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t summary --build-arg task=summarization --build-arg models="0 2 6" --build-arg arguments="0 input0 input1 input2 input3 input4 repeat 1" .
+docker build -t hugger --build-arg task=summarization --build-arg models="0 2 6" --build-arg arguments="0 input0 input1 input2 input3 input4 repeat 1" .
 docker build -t monitor --file Dockerfile-monitor .
--- compose-dump.sh
+++
@@ -1,5 +0,0 @@
-docker compose up -d
-docker wait summary-app-1
-tcpdump -r tcpdump_output/output.pcap
-docker compose down
-tcpdump -r tcpdump_output/output.pcap
